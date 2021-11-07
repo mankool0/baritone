902130ad3099625bc0a4dd8fdab7e76ae8bf0a37
Redhawk18 <redhawk76767676@gmail.com>
2021-11-07 01:11:29 -0400

    Reconfiged settings for better default values

--- src/api/java/baritone/api/Settings.java
+++ src/api/java/baritone/api/Settings.java
@@ -197,7 +197,24 @@ public final class Settings {
             Blocks.CHEST,
             Blocks.TRAPPED_CHEST,
             Blocks.STANDING_SIGN,
-            Blocks.WALL_SIGN
+            Blocks.WALL_SIGN,
+
+            Blocks.RED_SHULKER_BOX,
+            Blocks.ORANGE_SHULKER_BOX,
+            Blocks.YELLOW_SHULKER_BOX,
+            Blocks.LIME_SHULKER_BOX,
+            Blocks.GREEN_SHULKER_BOX,
+            Blocks.LIGHT_BLUE_SHULKER_BOX,
+            Blocks.CYAN_SHULKER_BOX,
+            Blocks.BLUE_SHULKER_BOX,
+            Blocks.PURPLE_SHULKER_BOX,
+            Blocks.MAGENTA_SHULKER_BOX,
+            Blocks.PINK_SHULKER_BOX,
+            Blocks.BROWN_SHULKER_BOX,
+            Blocks.WHITE_SHULKER_BOX,
+            Blocks.SILVER_SHULKER_BOX,
+            Blocks.GRAY_SHULKER_BOX,
+            Blocks.BLACK_SHULKER_BOX
     )));
 
     /**
@@ -313,7 +330,7 @@ public final class Settings {
     /**
      * Block reach distance
      */
-    public final Setting<Float> blockReachDistance = new Setting<>(4.5f);
+    public final Setting<Float> blockReachDistance = new Setting<>(4.0f);
 
     /**
      * How many degrees to randomize the pitch and yaw every tick. Set to 0 to disable
